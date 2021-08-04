3d4ab8f977786bc19a7467b1d6a652d1e17a7635
monsvt <60559185+monsvt@users.noreply.github.com>
2021-08-04 08:09:01 -0500

    Update mattermostNotify.groovy

--- vars/mattermostNotify.groovy
+++ vars/mattermostNotify.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def notification(String buildResult) {
+def call(String buildResult) {
   mattermostSend (color: "#2A42EE", message: "Build STARTED: ${env.JOB_NAME} #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Link to build>)")        
   if ( buildResult == "SUCCESS" ) {
     mattermostSend ( color: "good", message: "Build was SUCCESSFUL: ${env.JOB_NAME} #${env.BUILD_NUMBER} (<${env.BUILD_URL}|Link to build>)")
