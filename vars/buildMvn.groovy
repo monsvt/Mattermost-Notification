7e1300f4ea80d5ea02c0085841db57112d3628d9
monsvt <60559185+monsvt@users.noreply.github.com>
2021-08-09 16:07:44 -0500

    Create buildMvn.groovy

---
+++ vars/buildMvn.groovy
@@ -0,0 +1,24 @@
+def call(body) {
+    node {
+        stage('Checkout and set agent'){
+
+            echo "found the agentLabel value"
+        }
+    }
+
+    pipeline {
+        agent any
+        stages {
+            stage("echo parameters") {
+                steps {
+                    echo "hello"
+                }
+            }
+            
+      post {
+	        always {
+                mattermostNotify(currentBuild.currentResult)
+            }
+        }
+}
+}
